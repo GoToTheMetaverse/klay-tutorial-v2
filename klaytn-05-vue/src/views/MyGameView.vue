e58bf63661b78fc94ea67c60119ace936c52262a
gunil <this_is_one@naver.com>
2022-03-04 19:46:56 +0900

    코드 업데이트

--- klaytn-05-vue/src/views/MyGameView.vue
+++ klaytn-05-vue/src/views/MyGameView.vue
@@ -5,9 +5,9 @@
       <div v-if="!data.is_login">
         <button class="tw_btn" @click="onLogin()">LOGIN</button>
       </div>
-      <div v-else class="space-y-2">
+      <div v-else>
+        <div>지갑 연결 완료</div>
         <div>ADDR: {{ data.addr }}</div>
-        <button class="tw_btn" @click="onLoadContract()">onLoadContract</button>
         <div>balance_me: {{ data.balance_me }}</div>
         <div>blaance_contract: {{ data.blaance_contract }}</div>
         <div>bet_size: {{ data.bet_size }}</div>
@@ -24,7 +24,6 @@ export default {
   setup() {
     let caver = null;
     let contract = null;
-
     let data = reactive({
       is_login: false,
       addr: "",
@@ -42,34 +41,24 @@ export default {
       data.addr = acc[0];
 
       caver = new Caver(window.klaytn);
+      data.balance_me = await caver.klay.getBalance(data.addr);
+
       contract = new caver.klay.Contract(
         Contract.abi,
         Contract.networks["1001"].address
       );
-    };
 
-    const onLoadContract = async () => {
-      console.log("onLoadContract");
-
-      data.balance_me = await caver.klay.getBalance(data.addr);
       data.blaance_contract = await contract.methods.get_balance().call();
       data.bet_size = await contract.methods.get_bet_size().call();
     };
 
     return {
-      caver,
-      contract,
       data,
       //
       onLogin,
-      onLoadContract,
     };
   },
 };
 </script>
 
-<style scoped>
-.tw_btn {
-  @apply px-4 py-1 rounded-full bg-indigo-600 text-white;
-}
-</style>
+<style scoped></style>
--- klaytn-05-vue/vue.config.js
+++ klaytn-05-vue/vue.config.js
@@ -2,20 +2,20 @@
 module.exports = {
   chainWebpack: (config) => {
     config.plugin("html").tap((args) => {
-      args[0].title = "SIDNFT";
+      args[0].title = "코딩 자습서 - 클레이튼 가위바위보 게임";
       return args;
     });
   },
 
   pluginOptions: {
     i18n: {
-      locale: 'en',
-      fallbackLocale: 'en',
-      localeDir: 'locales',
+      locale: "en",
+      fallbackLocale: "en",
+      localeDir: "locales",
       enableLegacy: false,
       runtimeOnly: false,
       compositionOnly: false,
-      fullInstall: true
-    }
-  }
+      fullInstall: true,
+    },
+  },
 };
