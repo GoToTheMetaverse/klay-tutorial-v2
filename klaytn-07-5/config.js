c8cb0e7902a8d95b0bfa61f29b93982839e0b6cf
gunil <this_is_one@naver.com>
2022-05-11 00:10:44 +0900

    누락된 파일 추가

--- klaytn-07-5/config.js
+++ klaytn-07-5/config.js
@@ -2,5 +2,5 @@ module.exports = {
   clientId: "963338592489472010",
   clientSecret: "q5bwgiJHQBwnJYYhTxaRysruXZOEPf2t",
   port: 53134,
-  token: "OTYzMzM4NTkyNDg5NDcyMDEw.YlUo-g.qpJd7n5cxbKWcZp_iEw-Y6Q77ds",
+  token: "OTYzMzM4NTkyNDg5NDcyMDEw.YlUo-g.jZ0tqUXAwzrtsbTJM48Sezm1O40",
 };
