8ea314354fbae941414b603371f1787e355ff6b8
gunil <this_is_one@naver.com>
2022-06-11 23:12:14 +0900

    예제수정

--- klaytn-06/test.js
+++ klaytn-06/test.js
@@ -32,29 +32,43 @@ async function test() {
   const c_balance = ret;
   console.log("contract getBalance", ret);
   ret = await contract.methods.totalSupply().call();
+  const total = Number(ret);
   console.log("totalSupply", ret);
   ret = await contract.methods.balanceOf(addr).call();
   console.log("mynft balance", ret);
-  ret = await contract.methods.tokenURI(1).call();
-  console.log("tokenUri", ret);
+
+  if (total > 0) {
+    ret = await contract.methods.tokenURI(1).call();
+    console.log("tokenUri", ret);
+  } else {
+    console.log("tokenUri skip");
+  }
 
   ret = await contract.methods.getData().call();
   const fee = ret.fee;
   const count = ret.count;
   console.log("fee", fee);
-  console.log("count", count);
+  const left_mint_count = count - total;
+  console.log("남은 민팅 가능 수량", left_mint_count);
 
-  // await contract.methods.mintGeust().send({
-  //   from: addr,
-  //   gas: "20000000",
-  //   value: fee,
-  // });
+  // // 밍팅하기, 민팅가능 수량이 있어야 민팅할 수 있음
+  // if (left_mint_count > 0) {
+  //   console.log("try mintGeust");
+  //   ret = await contract.methods.mintGeust().send({
+  //     from: addr,
+  //     gas: "20000000",
+  //     value: fee,
+  //   });
+  //   console.log("result mintGeust", ret.events);
+  // }
 
+  // // 민팅 가능한 수량 설정
   // await contract.methods.setCount(10).send({
   //   from: addr,
   //   gas: "20000000",
   // });
 
+  // // 출급하기
   // await contract.methods.withdraw(c_balance).send({
   //   from: addr,
   //   gas: "20000000",
